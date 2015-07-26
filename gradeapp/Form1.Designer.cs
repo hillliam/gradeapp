2877d8ac1cdf6e77887877bf9ed68ce7bfe1a344
hillliam <b4026826@my.shu.ac.uk>
2015-07-26 20:45:07 +0100

    saveing and loading

--- gradeapp/Form1.Designer.cs
+++ gradeapp/Form1.Designer.cs
@@ -55,6 +55,7 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 95);
             this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button1
             // 
@@ -64,6 +65,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "load";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -73,6 +75,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "save";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button5
             // 
@@ -111,6 +114,7 @@
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(120, 95);
             this.listBox2.TabIndex = 8;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // button8
             // 
@@ -149,6 +153,7 @@
             this.listBox3.Name = "listBox3";
             this.listBox3.Size = new System.Drawing.Size(120, 95);
             this.listBox3.TabIndex = 12;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
             // 
             // listBox4
             // 
@@ -157,6 +162,7 @@
             this.listBox4.Name = "listBox4";
             this.listBox4.Size = new System.Drawing.Size(120, 95);
             this.listBox4.TabIndex = 13;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
             // 
             // button11
             // 
--- gradeapp/Form1.cs
+++ gradeapp/Form1.cs
@@ -7,12 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace gradeapp
 {
     public partial class Form1 : Form
     {
         private List<year> years;
+        private int selectedyear;
+        private int selectedsubject;
+        private int selectedexam;
+        private int selectedcoursework;
         public Form1()
         {
             InitializeComponent();
@@ -68,7 +73,7 @@ namespace gradeapp
 
         private void button13_Click(object sender, EventArgs e)
         {// add coursework
-
+            
         }
 
         private void button14_Click(object sender, EventArgs e)
@@ -91,5 +96,84 @@ namespace gradeapp
 
         }
 
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != 0)
+                selectedyear = listBox1.SelectedIndex;
+            else
+                selectedyear = -1;
+        }
+        private void updateyear()
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i != years.Count; i++)
+                listBox1.Items.Add(years[i].print());
+        }
+        private void updatesubjects()
+        {
+            listBox2.Items.Clear();
+            for (int i = 0; i != years[selectedyear].getsize(); i++)
+                listBox2.Items.Add(years[selectedyear].getsubject(i));
+        }
+        private void updateexams()
+        {
+            listBox4.Items.Clear();
+            for (int i = 0; i != years[selectedyear].getsubject(selectedsubject).examssize(); i++)
+                listBox4.Items.Add(years[selectedyear].getsubject(selectedsubject).getexam(i));
+        }
+        private void updatecoursework()
+        {
+            listBox3.Items.Clear();
+            for (int i = 0; i != years[selectedyear].getsubject(selectedsubject).courseworksize(); i++)
+                listBox3.Items.Add(years[selectedyear].getsubject(selectedsubject).getcoursework(i));
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != 0)
+                selectedsubject = listBox2.SelectedIndex;
+            else
+                selectedsubject = -1;
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { // load
+            OpenFileDialog b = new OpenFileDialog();
+            if (b.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader a = new StreamReader(b.FileName);
+                int numofyears = int.Parse(a.ReadLine());
+                for (int i = 0; i != numofyears; i++)
+                {
+                    year c = new year();
+                    c.loadsubjects(ref a);
+                    years.Add(c);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { // save
+            SaveFileDialog a = new SaveFileDialog();
+            if (a.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter b = new StreamWriter(a.FileName);
+                b.WriteLine(years.Count);
+                for (int i = 0; i != years.Count; i++ )
+                {
+                    years[i].savesubject(ref b);
+                }
+            }
+        }
     }
 }
